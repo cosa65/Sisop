984bc47aeb1858d5c986d8cb1160140af20c5ce6
nicoroulet <nicoroulet@gmail.com>
2015-06-04 20:59:42 -0300

    andaaa

--- Tp2/tp2/src/backend-multi/RWLock.cpp
+++ Tp2/tp2/src/backend-multi/RWLock.cpp
@@ -1,16 +1,21 @@
 #include "RWLock.h"
 
-RWLock :: RWLock() {
+RWLock :: RWLock() : rwlock(){
 }
 
 void RWLock :: rlock() {
+	pthread_rwlock_rdlock(&rwlock);
 }
 
 void RWLock :: wlock() {
+
+	pthread_rwlock_wrlock(&rwlock);
 }
 
 void RWLock :: runlock() {
+	pthread_rwlock_unlock(&rwlock);
 }
 
 void RWLock :: wunlock() {
+	pthread_rwlock_unlock(&rwlock);
 }
